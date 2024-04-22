145a8a98417fcffe2d47ec99f6f161bd4caf87a0
ilya-4real <ilya.chernishyov@gmail.com>
2024-04-22 21:06:43 +0200

    started refactoring this shit

--- backend/deck.py
+++ backend/deck.py
@@ -1,24 +1,37 @@
-
 import random
 
 
-DECK_FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
-DECK_SUITS = ['diamonds', 'clubs', 'hearts', 'spades' ]
+DECK_FACES = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+    "ace",
+]
+DECK_SUITS = ["diamonds", "clubs", "hearts", "spades"]
 
-VALUES  = {
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    '10': 10,
-    'jack': 10,
-    'queen': 10,
-    'king': 10,
-    'ace': [1, 11]
+VALUES = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "jack": 10,
+    "queen": 10,
+    "king": 10,
+    "ace": [1, 11],
 }
 
 
@@ -28,5 +41,6 @@ def generate_random_card():
         suit = random.choice(DECK_SUITS)
         value = VALUES[face]
         yield (face, value, suit)
+
 
 card_generator = generate_random_card()
