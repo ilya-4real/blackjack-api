8591917e7a124ed323d463c1f017fe1f9490385a
ilya-4real <ilya.chernishyov@gmail.com>
2024-05-05 21:07:11 +0200

    fixed compose file to mount dir into container

--- backend/app/application/main.py
+++ backend/app/application/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
+from app.application.auth.router import router as auth_router
+
 
 def get_app() -> FastAPI:
     app = FastAPI(
         title="simple application for blackjack game",
         docs_url="/docs",
     )
+    app.include_router(auth_router)
     return app
